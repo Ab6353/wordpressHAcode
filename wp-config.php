3296a1cd0dd7f1c9c59495ad8f6ce97b611211ae
Ashutosh Behera <91386098+Ab6353@users.noreply.github.com>
2022-03-21 11:55:41 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php
-//define( 'WP_HOME', 'https://sample.squareops.co.in' );
-//define( 'WP_SITEURL', 'https://sample.squareops.co.in' );
-//if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)   $_SERVER['HTTPS']='on';
+define( 'WP_HOME', 'https://samplecicd.squareops.co.in' );
+define( 'WP_SITEURL', 'https://samplecicd.squareops.co.in' );
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)   $_SERVER['HTTPS']='on';
 /**
  * The base configuration for WordPress
  *
