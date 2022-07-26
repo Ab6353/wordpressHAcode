99d44251cd320247b3becbd71f0a4cbc2ad9e0f4
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-07-26 11:36:35 +0530

    Update validate-server.sh

--- scripts/validate-server.sh
+++ scripts/validate-server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl wpcicd.squareops.co.in
+//curl wpcicd.squareops.co.in
+RESPONSE="$(curl wpcicd.squareops.co.in)"
+if [[ "$RESPONSE" == *"200"* ]]; then echo "App running"; else echo "Not running";
+fi
